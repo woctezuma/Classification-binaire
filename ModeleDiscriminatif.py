4270b9b9d81c06a397e3414adad68d685666d11a
Wok <woctezuma@users.noreply.github.com>
2018-05-13 10:19:53 +0200

    Formatting

--- ModeleDiscriminatif.py
+++ ModeleDiscriminatif.py
@@ -1,22 +1,25 @@
 import csv
+
 import numpy as np
+
 from Modele import Modele
 
+
 class ModeleDiscriminatif(Modele):
-        '''
-        Modele discriminatif de classification binaire
-        '''
-        
-        def __init__(self):
-                Modele.__init__(self)
-                self.beta = np.array(0)
-                return
-        
-        def creer(self, filename, sep ='\t'):
-                fileReader = csv.reader(open(filename),delimiter=sep)
-                fileContent = [[float(xa),float(xb),float(y)]
-                               for (xa,xb,y) in fileReader]
-                XY = np.array(fileContent)
-                Xintercept =  np.linspace(1,1,len(XY))
-                self.X = np.column_stack((Xintercept, XY[:,:-1]))
-                self.Y = XY[:, -1]
+    '''
+    Modele discriminatif de classification binaire
+    '''
+
+    def __init__(self):
+        Modele.__init__(self)
+        self.beta = np.array(0)
+        return
+
+    def creer(self, filename, sep='\t'):
+        fileReader = csv.reader(open(filename), delimiter=sep)
+        fileContent = [[float(xa), float(xb), float(y)]
+                       for (xa, xb, y) in fileReader]
+        XY = np.array(fileContent)
+        Xintercept = np.linspace(1, 1, len(XY))
+        self.X = np.column_stack((Xintercept, XY[:, :-1]))
+        self.Y = XY[:, -1]
--- ModeleGeneratif.py
+++ ModeleGeneratif.py
@@ -1,6 +1,9 @@
 ﻿import csv
+
 import numpy as np
+
 from Modele import Modele
+
 
 class ModeleGeneratif(Modele):
     '''
@@ -13,11 +16,11 @@ class ModeleGeneratif(Modele):
         self.mu1 = np.array(0)
         self.mu0 = np.array(0)
         return
-    
-    def creer(self, filename, sep ='\t'):
-        fileReader = csv.reader(open(filename),delimiter=sep)
-        fileContent = [[float(xa),float(xb),float(y)]
-                       for (xa,xb,y) in fileReader]
+
+    def creer(self, filename, sep='\t'):
+        fileReader = csv.reader(open(filename), delimiter=sep)
+        fileContent = [[float(xa), float(xb), float(y)]
+                       for (xa, xb, y) in fileReader]
         XY = np.array(fileContent)
-        self.X = XY[:,:-1]
+        self.X = XY[:, :-1]
         self.Y = XY[:, -1]
