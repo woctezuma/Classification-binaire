2a5813624bbc369a5ab2ec9707131b9542b71869
Wok <woctezuma@users.noreply.github.com>
2018-05-13 10:20:19 +0200

    Unit tests

--- tests.py
+++ tests.py
@@ -1,6 +1,41 @@
 import unittest
 
+import load_data
 import main
+from LDA import LDA
+from QDA import QDA
+from RegressionLineaire import RegressionLineaire
+from RegressionLogistique import RegressionLogistique
+
+
+class TestLoadDataMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(load_data.main())
+
+
+class TestRegressionLineaireMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(RegressionLineaire.main())
+
+
+class TestRegressionLogistiqueMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(RegressionLogistique.main())
+
+
+class TestLDAMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(LDA.main())
+
+
+class TestQDAMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(QDA.main())
 
 
 class TestMainMethods(unittest.TestCase):
