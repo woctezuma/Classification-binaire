4e0ec18717f049280fcfa0d820b185e0c4c57b7c
Wok <woctezuma@users.noreply.github.com>
2018-05-13 10:20:10 +0200

    Use load_data

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from RegressionLineaire import RegressionLineaire
 from RegressionLogistique import RegressionLogistique
-from LDA import LDA
-from QDA import QDA
 
 
 def main():
-    train_filename = 'myTrainFile.txt'  # TODO
-    test_filename = 'myTestFile.txt'  # TODO
-    suffixe_graphique = '.RegLog.png'
+    from load_data import get_train_filename, get_test_filename, get_suffixe_graphique
 
-    my_classifier = RegressionLogistique()
-    my_classifier.train(train_filename)
+    for dataset_letter in ['A', 'B', 'C']:
+        my_classifier = RegressionLogistique()
+        my_classifier.train(get_train_filename(dataset_letter))
 
-    my_classifier.afficherErreur(train_filename)
-    my_classifier.afficher(train_filename + suffixe_graphique)
+        my_classifier.afficherErreur(get_train_filename(dataset_letter))
+        my_classifier.afficher(get_train_filename(dataset_letter) + get_suffixe_graphique(my_classifier.get_name()))
 
-    my_classifier.afficherErreur(test_filename)
-    my_classifier.afficher(test_filename + suffixe_graphique)
+        my_classifier.afficherErreur(get_test_filename(dataset_letter))
+        my_classifier.afficher(get_test_filename(dataset_letter) + get_suffixe_graphique(my_classifier.get_name()))
 
     return True
+
 
 if __name__ == "__main__":
     main()
