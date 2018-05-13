b5f65b3b17c3bfb3fa98bf901e6d8d96a92d3adc
Wok <woctezuma@users.noreply.github.com>
2018-05-13 10:19:35 +0200

    Set and get name in super class

--- Modele.py
+++ Modele.py
@@ -1,40 +1,47 @@
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Modele:
     '''
     Modele pour la classification binaire
     '''
-    
+
     def __init__(self):
         self.X = np.array(0)
         self.Y = np.array(0)
+        self.name = self.set_name()
         return
-    
-    def creer(self, filename, sep ='\t'):
+
+    def set_name(self):
         raise NotImplemented
-    
+
+    def get_name(self):
+        return self.name
+
+    def creer(self, filename, sep='\t'):
+        raise NotImplemented
+
     def train(self, filename):
         raise NotImplemented
-    
+
     def getErreur(self, filename):
         raise NotImplemented
-    
+
     def getSeparateur(self):
         raise NotImplemented
-    
+
     def afficher(self, outputName):
         plt.figure()
-        labels_1 = (self.Y==1)
-        plt.plot(self.X[labels_1,-2], self.X[labels_1,-1], 'bo')
-        labels_0 = (self.Y==0)
-        plt.plot(self.X[labels_0,-2], self.X[labels_0,-1], 'ro')
+        labels_1 = (self.Y == 1)
+        plt.plot(self.X[labels_1, -2], self.X[labels_1, -1], 'bo')
+        labels_0 = (self.Y == 0)
+        plt.plot(self.X[labels_0, -2], self.X[labels_0, -1], 'ro')
         (abscisse, ordonnee) = self.getSeparateur()
         plt.plot(abscisse, ordonnee, 'k')
         plt.savefig(outputName)
         return
-    
+
     def afficherErreur(self, filename):
-        print 'Erreur (%s) : %.2f' % (filename, self.getErreur(filename))
+        print('Erreur ({}) : {:.2f}'.format(filename, self.getErreur(filename)))
         return
